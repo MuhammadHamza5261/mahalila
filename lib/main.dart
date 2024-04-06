91aee96579cdbaaccc137117a196669b307c52c9
Muzammil Khan <engrmuzzamil.kh88@gmail.com>
2024-04-06 22:12:16 +0500

    many screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mahalila/bottom_navigator.dart';
+import 'package:mahalila/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: NavigationScreen(),
+      home: SplashScreen(),
     );
   }
 }
